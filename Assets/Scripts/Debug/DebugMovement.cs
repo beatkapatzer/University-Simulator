3cc37ab528e86c4f4fe630a7f529d74d86aad4ae
PrzemyslawStefanski <przemyslaw@yazagames.com>
2024-11-12 19:27:48 +0100

    Added first canvas and debug movement, additionaly ceiling+testcube to the map

---
+++ Assets/Scripts/Debug/DebugMovement.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework.Internal;
+using UnityEngine;
+
+public class DebugMovement : MonoBehaviour
+{
+
+    #region "Variables"
+    public Rigidbody Rigid;
+    public float MouseSensitivity;
+    public float MoveSpeed;
+    public float JumpForce;
+
+    #endregion
+
+    void Update()
+    {
+        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
+        Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
+        if (Input.GetKeyDown("space"))
+            Rigid.AddForce(transform.up * JumpForce);
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
