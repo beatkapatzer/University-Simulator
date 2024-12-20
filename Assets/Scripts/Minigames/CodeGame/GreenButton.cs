b0eafbf16b3a266df9508c68e9bfca2483187e15
Paweł 78209 <psozanskii@gmail.com>
2024-12-20 20:59:34 +0100

    Zielony Przycisk

---
+++ Assets/Scripts/Minigames/CodeGame/GreenButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CheckButton : MonoBehaviour
+{
+    public InputOutputField[] inputFields;
+    public InputOutputField[] outputFields;
+
+    public void OnCheckClick()
+    {
+        bool isCorrect = true;
+
+        for (int i = 0; i < inputFields.Length; i++)
+        {
+            if (inputFields[i].value != outputFields[i].value)
+            {
+                isCorrect = false;
+                outputFields[i].fieldValue.color = Color.red;
+            }
+            else
+            {
+                outputFields[i].fieldValue.color = Color.green;
+            }
+        }
+
+        if (isCorrect)
+        {
+            Debug.Log("Konto zosta³o zhakowane poprawnie!");
+        }
+    }
+}
